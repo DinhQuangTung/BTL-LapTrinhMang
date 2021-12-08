a11d10d0682d9fd9bc0f85e0210f4a3a11d62f2a
tungdq <dinhquangtung6954@gmail.com>
2021-12-08 20:24:57 +0700

    update protocol

--- src/Publisher/Publisher.java
+++ src/Publisher/Publisher.java
@@ -1,9 +1,16 @@
 package Publisher;
 
 import GlobalVariable.*;
+import java.util.*;
 
 public class Publisher implements Locations, Topics {
-    public static void main(String[] args) throws Exception {
+    static int createMode() {
+        Random rand = new Random();
+        return rand.nextInt(2);
+    }
 
+    public static void main(String[] args) throws Exception {
+        int mode = createMode();
+        System.out.println(mode);
     }
 }
